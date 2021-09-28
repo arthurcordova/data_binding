c2b887b0053d63c7bbbc0481a243e31e2947cfed
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2021-09-28 13:04:38 -0300

    data binding

---
+++ app/src/main/java/com/proway/databingingapp/ui/main/MainFragment.kt
@@ -0,0 +1,43 @@
+package com.proway.databingingapp.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.proway.databingingapp.R
+import com.proway.databingingapp.databinding.MainFragmentBinding
+import com.proway.databingingapp.BR.mainViewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.main_fragment, container, false);
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.mainViewModel = viewModel
+
+    }
+
+}
